223a4c3891455c2253d6b575cb5badee8c02c272
lesjaek-lab <lesjaek@gmail.com>
2026-04-01 15:10:03 -0600

    First commit!
    First commit!

---
+++ untitled0.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 17:27:57 2026
+
+@author: leslikey
+"""
+
+# distance of channel (x) is 1000 feet 
+
+#Define gamma parameters, h,b,z. z=0 is rect. 
+#h = height, b = bottom width, z = side slope
+#We need at least 2 diff geoms
+h1 = 6
+b1 = 8
+z1 = 0
+
+h2 = 3
+b2 = 5
+z1 = 9
+
+#area of geom 1
+A1 = h1*b1
+
+#irregular channel is episilon and something else 
+
+#flow input in cfs (flow is steady), u
+Qin = 100
+
